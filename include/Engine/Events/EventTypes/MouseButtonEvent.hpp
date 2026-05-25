8bfba761f74d0b50dc26cb716f4714c3d17c7f72
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-05-25 01:44:58 -0400

    Add position to MouseButtonEvent

--- include/Engine/Events/EventTypes/MouseButtonEvent.hpp
+++ include/Engine/Events/EventTypes/MouseButtonEvent.hpp
@@ -9,6 +9,7 @@
 #include "Events/EngineEvent.hpp"
 #include "Types/Vector2.hpp"
 #include <SDL3/SDL.h>
+#include <iostream>
 
 enum class MouseButton {
     LEFT = SDL_BUTTON_LEFT,
@@ -19,13 +20,15 @@ enum class MouseButton {
 struct MouseButtonEvent : public EngineEvent {
     MouseButton button;
     bool pressed;
+    Vector2i position;
 
     /**
      * @brief Constructor
      * 
      * @param button The mouse button this action is about
      * @param pressed If the mouse button is pressed or not
+     * @param position The mouse position of the click
      */
-    MouseButtonEvent(MouseButton button, bool pressed)
-        : button(button), pressed(pressed) {}
+    MouseButtonEvent(MouseButton button, bool pressed, const Vector2i& position)
+        : button(button), pressed(pressed), position(position) {}
 };

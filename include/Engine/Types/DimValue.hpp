e3b59411696ebe5873e31f7b9998f668d9cddf1e
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-05-25 01:51:01 -0400

    Add ostream insertion

--- include/Engine/Types/DimValue.hpp
+++ include/Engine/Types/DimValue.hpp
@@ -9,4 +9,17 @@
 struct DimValue {
     double scale = 0.5;
     int offset = 0;
+
+    /***
+     * @brief Overloaded insertion operator
+     * 
+     * @param o A reference to an output stream
+     * @param dval The DimValue being output to the stream
+     * 
+     * @result A reference to the output stream being output to
+     */
+    friend std::ostream& operator<<(std::ostream& o, const DimValue& dval) {
+        o << "[" << dval.scale << ", " << dval.offset << "]";
+        return o;
+    }
 };
--- include/Engine/Types/DimVector.hpp
+++ include/Engine/Types/DimVector.hpp
@@ -11,4 +11,17 @@
 struct DimVector {
     DimValue x;
     DimValue y;
+
+    /***
+     * @brief Overloaded insertion operator
+     * 
+     * @param o A reference to an output stream
+     * @param dvec The DimVector being output to the stream
+     * 
+     * @result A reference to the output stream being output to
+     */
+    friend std::ostream& operator<<(std::ostream& o, const DimVector& dvec) {
+        o << "[" << dvec.x << ", " << dvec.y << "]";
+        return o;
+    }
 };

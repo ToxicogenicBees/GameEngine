8db831bc4efd41877c75df633ab1b1cd9ca72662
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-05-27 14:21:52 -0400

    Add hash functionality

--- include/Engine/Types/Vector2.hpp
+++ include/Engine/Types/Vector2.hpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include <type_traits>
 #include <ostream>
+#include <functional>
 
 template <typename T>
 struct Vector2_;
@@ -20,6 +21,16 @@ using Vector2d = Vector2_<double>;
 
 template <typename A, typename B>
 using promote_t = std::common_type_t<A, B>;
+
+template <typename T>
+struct std::hash<Vector2_<T>> {
+    std::size_t operator()(const Vector2_<T>& v) const noexcept {
+        std::size_t h1 = std::hash<T>{}(v.x);
+        std::size_t h2 = std::hash<T>{}(v.y);
+
+        return h1 ^ (h2 << 1);
+    }
+};
 
 template <typename T>
 struct Vector2_ {

73cdf44cd4b4a2e0a9b3ff0721cbbcaa232dcab3
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-06-02 17:39:23 -0400

    Remove path access from assets

--- include/Engine/Assets/Asset.hpp
+++ include/Engine/Assets/Asset.hpp
@@ -9,28 +9,16 @@
 #include <filesystem>
 
 class Asset {
-private:
-    std::filesystem::path PATH_;
-
 public:
     /**
      * @brief Constructor.
-     * 
-     * @param path The file path of this asset
      */
-    Asset(const std::filesystem::path& path);
+    Asset() = default;
 
     /**
      * @brief Copy constructor (deleted)
      */
     Asset(const Asset&) = delete;
-
-    /**
-     * @brief Gets the file path of the asset.
-     * 
-     * @return The file path of the asset.
-     */
-    std::filesystem::path path() const;
 
     /**
      * @brief Destructor.

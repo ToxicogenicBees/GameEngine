45baacfb4040d6110d744dd8c12d8f2fee2804c1
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-07-27 19:15:02 -0400

    Reorganize workspace

---
+++ include/Engine/Core/Containers/Interfaces/IResolver.hpp
@@ -0,0 +1,28 @@
+/*
+    IResolver.hpp
+
+    An interface for handle resolvers
+*/
+
+#pragma once
+
+#include "Core/Containers/Handle.hpp"
+
+namespace toxico {
+    template<typename Handle_t, typename Result_t>
+    class IResolver {
+    public:
+        /**
+         * @brief Resolve the provided handle.
+         * 
+         * @param handle The handle being resolved.
+         * @return The resolved type
+         */
+        virtual Result_t* resolve(Handle_t handle) = 0;
+
+        /**
+         * @brief Destructor.
+         */
+        virtual ~IResolver() = default;
+    };
+}

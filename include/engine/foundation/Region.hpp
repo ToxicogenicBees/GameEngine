39cda2301d5ff84ca301a34a76cd3fc24fcbf933
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-08-18 13:44:30 -0400

    Add Region utility

---
+++ include/engine/foundation/Region.hpp
@@ -0,0 +1,81 @@
+/*
+    Region.hpp
+
+    Declaration for an iterable 2D planar region.
+*/
+
+#pragma once
+
+#include "core/math/Vector2.hpp"
+#include "core/geometry/Size.hpp"
+
+namespace toxico {
+    class RegionIterator {
+    private:
+        Vector2i current_;
+        Vector2i begin_;
+        Vector2i end_;
+
+    public:
+        /**
+         * @brief Constructor.
+         * 
+         * @param current The current position of the iterator.
+         * @param begin The starting point of the Region.
+         * @param end The end point of the Region.
+         */
+        RegionIterator(Vector2i current, Vector2i begin, Vector2i end) noexcept;
+
+        /**
+         * @brief Dereference operator.
+         * 
+         * @return The position of this iterator.
+         */
+        Vector2i operator*() const noexcept;
+
+        /**
+         * @brief Post-increment operator.
+         * 
+         * @return The incremented iterator.
+         */
+        RegionIterator& operator++() noexcept;
+
+        /**
+         * @brief Comparison operator.
+         * 
+         * @param other Another Region iterator
+         * @return If the two iterators are the same.
+         */
+        bool operator==(const RegionIterator& other) const noexcept;
+    };
+
+    class Region {
+    private:
+        Vector2i begin_;
+        Vector2i end_;
+
+    public:
+        /**
+         * @brief Constructor.
+         * 
+         * @param begin The starting point of the Region.
+         * @param size The size the Region.
+         */
+        Region(Vector2i begin, Size size) noexcept;
+
+        /**
+         * @brief Constructor.
+         * 
+         * @param size The size the Region.
+         */
+        Region(Size size) noexcept;
+
+        /**
+         * @brief Gets the desired iterator for this Region.
+         * 
+         * @return The desired iterator.
+         */
+        RegionIterator begin() const noexcept;
+        RegionIterator end() const noexcept;
+    };
+}
---
+++ src/engine/foundation/Region.cpp
@@ -0,0 +1,49 @@
+/*
+    Region.cpp
+
+    Implementation for an iterable 2D planar region.
+*/
+
+#include "foundation/Region.hpp"
+
+namespace toxico {
+    RegionIterator::RegionIterator(Vector2i current, Vector2i begin, Vector2i end) noexcept
+        : current_(current), begin_(begin), end_(end) {}
+
+    Vector2i RegionIterator::operator*() const noexcept {
+        return current_;
+    }
+
+    RegionIterator& RegionIterator::operator++() noexcept {
+        ++current_.x;
+
+        if (current_.x >= end_.x) {
+            current_.x = begin_.x;
+            ++current_.y;
+        }
+
+        return *this;
+    }
+
+    bool RegionIterator::operator==(const RegionIterator& other) const noexcept {
+        return current_ == other.current_;
+    }
+
+    Region::Region(Vector2i begin, Size size) noexcept
+        : begin_(begin),
+          end_(begin + Vector2i{static_cast<int>(size.width), static_cast<int>(size.height)})
+    {}
+
+    Region::Region(Size size) noexcept
+        : begin_(Vector2i::zero()),
+          end_(Vector2i{static_cast<int>(size.width), static_cast<int>(size.height)})
+    {}
+
+    RegionIterator Region::begin() const noexcept {
+        return {begin_, begin_, end_};
+    }
+
+    RegionIterator Region::end() const noexcept {
+        return {{begin_.x, end_.y}, begin_, end_};
+    }
+}

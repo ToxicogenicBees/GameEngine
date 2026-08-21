7a01021fdb144347bb3bf09611fc44aee12af4eb
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-08-21 03:11:00 -0400

    Add TypeMap container

---
+++ include/engine/foundation/containers/TypeMap.hpp
@@ -0,0 +1,78 @@
+/*
+    TypeMap.hpp
+
+    Declaration of a map, a collection of objects of the same base type.
+*/
+
+#pragma once
+
+#include <type_traits>
+#include <unordered_map>
+#include <typeindex>
+#include <memory>
+
+namespace toxico {
+    template<typename Base>
+    class TypeMap {
+    private:
+        std::unordered_map<std::type_index, std::unique_ptr<Base>> data_;
+
+    public:
+        /**
+         * @brief Inserts an item into the map.
+         * 
+         * @param value The item being inserted.
+         * 
+         * Throws a runtime exception if an item of this type already exists.
+         */
+        template<typename Derived, typename... Args>
+        requires std::is_base_of_v<Base, Derived>
+        Derived* insert(std::unique_ptr<Derived> value);
+    
+        /**
+         * @brief Emplaces an item into the map.
+         * 
+         * @param args The template arguments for this item.
+         * 
+         * Throws a runtime exception if an item of this type already exists.
+         */
+        template<typename Derived, typename... Args>
+        requires std::is_base_of_v<Base, Derived>
+        Derived* emplace(Args&& ...args);
+
+        /**
+         * @brief Gets if an item is in the map.
+         * 
+         * @return If the item is in the map.
+         */
+        template<typename Derived>
+        bool contains() const noexcept;
+
+        /**
+         * @brief Gets an item to the map.
+         * 
+         * @return The item, or nullptr if it wasn't contained in the map.
+         */
+        template<typename Derived>
+        requires std::is_base_of_v<Base, Derived>
+        const Derived* get() const noexcept;
+
+        /**
+         * @brief Gets an item to the map.
+         * 
+         * @return The item, or nullptr if it wasn't contained in the map.
+         */
+        template<typename Derived>
+        requires std::is_base_of_v<Base, Derived>
+        Derived* get() noexcept;
+
+        /**
+         * @brief Removes an item from the map.
+         */
+        template<typename Derived>
+        requires std::is_base_of_v<Base, Derived>
+        void erase() noexcept;
+    };
+}
+
+#include "foundation/containers/TypeMap.tpp"
---
+++ src/engine/foundation/containers/TypeMap.tpp
@@ -0,0 +1,64 @@
+/*
+    TypeMap.tpp
+
+    Template implementation of a map, a collection of objects of the same base type.
+*/
+
+#include <utility>
+
+namespace toxico {
+    template<typename Base>
+    template<typename Derived, typename... Args>
+    requires std::is_base_of_v<Base, Derived>
+    Derived* TypeMap<Base>::insert(std::unique_ptr<Derived> value) {
+        if (contains<Derived>())
+            throw std::runtime_error("TypeMap cannot add an item of a type it already contains");
+
+        data_[typeid(Derived)] = std::move(value);
+        return data_[typeid(Derived)].get();
+    }
+    
+    template<typename Base>
+    template<typename Derived, typename... Args>
+    requires std::is_base_of_v<Base, Derived>
+    Derived* TypeMap<Base>::emplace(Args&& ...args) {
+        if (contains<Derived>())
+            throw std::runtime_error("TypeMap cannot add an item of a type it already contains");
+
+        data_[typeid(Derived)] = std::make_unique<Derived>(std::forward<Args>(args)...);
+        return data_[typeid(Derived)].get();
+    }
+
+    template<typename Base>
+    template<typename Derived>
+    bool TypeMap<Base>::contains() const noexcept {
+        return data_.contains(typeid(Derived));
+    }
+
+    template<typename Base>
+    template<typename Derived>
+    requires std::is_base_of_v<Base, Derived>
+    const Derived* TypeMap<Base>::get() const noexcept {
+        if (!contains<Derived>())
+            return nullptr;
+
+        return static_cast<Derived*>(data_[typeid(Derived)].get());
+    }
+
+    template<typename Base>
+    template<typename Derived>
+    requires std::is_base_of_v<Base, Derived>
+    Derived* TypeMap<Base>::get() noexcept {
+        if (!contains<Derived>())
+            return nullptr;
+
+        return static_cast<Derived*>(data_.at(typeid(Derived)).get());
+    }
+
+    template<typename Base>
+    template<typename Derived>
+    requires std::is_base_of_v<Base, Derived>
+    void TypeMap<Base>::erase() noexcept {
+        data_.erase(typeid(Derived));
+    }
+}

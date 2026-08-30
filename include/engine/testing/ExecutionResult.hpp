7e178f81dea7bda727e88c9c29482ae05e3aef26
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-08-30 15:49:09 -0400

    Add output stream insertion overload to execution result

--- include/engine/testing/ExecutionResult.hpp
+++ include/engine/testing/ExecutionResult.hpp
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <ostream>
 #include <string>
 
 namespace toxico::test {
@@ -14,4 +15,14 @@ namespace toxico::test {
         std::string info = "N/A";
         double microseconds = 0;
     };
+
+    /**
+     * @brief Write an execution result to an output stream
+     * 
+     * @param o The output stream being written to.
+     * @param result The result being written.
+     * 
+     * @return A reference to the modified output stream.
+     */
+    std::ostream& operator<<(std::ostream& o, const ExecutionResult& result);
 }
---
+++ src/engine/testing/ExecutionResult.cpp
@@ -0,0 +1,16 @@
+/*
+    ExecutionResult.cpp
+
+    Implementation of a struct containing test execution data.
+*/
+
+#include "testing/ExecutionResult.hpp"
+
+namespace toxico::test {
+    std::ostream& operator<<(std::ostream& o, const ExecutionResult& result) {
+        o << "Result: " << (result.success ? "Passed" : "Failed") << "\n";
+        o << "Time:   " << result.microseconds * 1e-3 << " ms\n";
+        o << result.info;
+        return o;
+    }
+}
